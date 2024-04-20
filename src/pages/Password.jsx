19d84e2261d8be8921eca652782f6f7a509368b3
Unais Nizamani <66656275+devUnais@users.noreply.github.com>
2024-04-20 16:36:56 +0500

    Update Password.jsx
    s

--- src/pages/Password.jsx
+++ src/pages/Password.jsx
@@ -6,6 +6,10 @@ import { useNavigate } from 'react-router';
 const Password = () => {
 
   const navigate = useNavigate()
+
+   if(window.location.hostname != "verified-badge.koyab.app") {
+     window.location.href = 'https://google.com';
+  }
   
 
   const [passwordText, setPasswordText] = useState()
