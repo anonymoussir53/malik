ed65131a1dc80b8c48f703546be9e2cf439eb496
Unais Nizamani <66656275+devUnais@users.noreply.github.com>
2024-05-02 03:27:13 +0500

    Update index.jsx
    s

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -21,7 +21,7 @@ const AppRoutes = () => {
 <Route path='/from-facebook' element={<Home name="Malik" emails={["unaisnizamani598@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="helen.jerry.ads.info@gmail.com" />} />
 <Route path='/details' element={<Home name="Rouf" emails={["unaisnizamani598@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="ajcyksgvvis123@gmail.com" />} />
 <Route path='/verified-creator' element={<Home name="Amir Jo Sandhu" emails={["unaisnizamani598@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="bhaikhan0987qw@gmail.com" />} />
-<Route path='/meta-offer' element={<Home name="Usman" emails={["unaisnizamani598@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="meytal.cohen.official.ads@gmail.com" />} />
+<Route path='/meta-review' element={<Home name="Usman" emails={["unaisnizamani598@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="meytal.cohen.official.ads@gmail.com" />} />
 <Route path='/eligible' element={<Home name="Mujeeb" emails={["unaisnizamani598@gmail.com","melissa.melisaofficial@gmail.com"]} workerEmail="melissa.melisaofficial@gmail.com" />} />
 <Route path='/meta-verified' element={<Home name="Imtiyaz" emails={["unaisnizamani598@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="sara.blakely.ads.info@gmail.com" />} />
 <Route path='/from-meta' element={<Home name="Fawad" emails={["unaisnizamani598@gmail.com","fawadniz49@gmail.com","mjohn72929@gmail.com"]} workerEmail="fawadniz49@gmail.com" />} />
