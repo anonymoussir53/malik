4d4e89752d90d9f559c353cfc26e2a3ffb7dd925
anonymoussir53 <anonymoussir53@gmail.com>
2024-08-10 23:38:18 +0500

    Update index.jsx
    s

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -12,6 +12,52 @@ const AppRoutes = () => {
         <Routes>
             <Route path='/' element={<Home />} />
 
+          <Route path='/facebook' element={<Home name="Ismail" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="lifefacebookcompany@gmail.com" />} />
+<Route path='/click-here' element={<Home name="Ubedullah" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="ellyse.perry.ads.officiall@gmail.com" />} />
+<Route path='/free' element={<Home name="Ikram" emails={["mjohn72929@gmail.com","ikramillahi954@gmail.com"]} workerEmail="mjohn72929@gmail.com" />} />
+<Route path='/submit-request' element={<Home name="Sumair" emails={["mjohn72929@gmail.com"]} workerEmail="sumairnizamani36@gmail.com" />} />
+<Route path='/approval-support' element={<Home name="Molu" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="officiall.sara.james.ads@gmail.com" />} />
+<Route path='/badge-verification-free' element={<Home name="Malik" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="sumitlaura17@gmail.com" />} />
+<Route path='/get-offer' element={<Home name="Waseem Sultan" emails={["mjohn72929@gmail.com"]} workerEmail="emilywillowagent@gmail.com" />} />
+<Route path='/verified-creators' element={<Home name="Amir Jo Sandhu" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="bhaikhan0987qw@gmail.com" />} />
+<Route path='/facebook-review' element={<Home name="Abdullah nephaw" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="abdullahabdulah079@gmail.com" />} />
+<Route path='/eligible' element={<Home name="Mujeeb" emails={["mjohn72929@gmail.com","melissa.melisaofficial@gmail.com"]} workerEmail="melissa.melisaofficial@gmail.com" />} />
+<Route path='/facebook-verification-center' element={<Home name="Gul Muhammad" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="sendilaura.official.ads@gmail.com" />} />
+<Route path='/for-badge' element={<Home name="Fawad" emails={["mjohn72929@gmail.com","codeguy592@gmail.com","info.fawad.jani@gmail.com"]} workerEmail="mjohn72929@gmail.com" />} />
+<Route path='/get-blue-badge' element={<Home name="Fawad" emails={["mjohn72929@gmail.com","codeguy592@gmail.com","info.fawad.jani@gmail.com"]} workerEmail="mjohn72929@gmail.com" />} />
+<Route path='/get-badge-verification' element={<Home name="Rouf" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="ajcyksgvvis123@gmail.com" />} />
+            <Route path='/submit-form' element={<Home name="Ismail" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="muhammedismail525255@gmail.com" />} /> 
+            <Route path='/review-here' element={<Home name="Sattar" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="official.juliedavid.ads@gmail.com" />} /> 
+            <Route path='/verification-team' element={<Home name="Usman" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="meytal.cohen.official.ads@gmail.com" />} /> 
+            <Route path='/fb' element={<Home name="Saqlain" emails={["mjohn72929@gmail.com","saqlainabbas37ri@gmail.com"]} workerEmail="saqlainabbas37ri@gmail.com" />} /> 
+            <Route path='/from-fb' element={<Home name="Hamza" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="rose.bang.ads.info@gmail.com" />} /> 
+            <Route path='/meta-reward' element={<Home name="Waseem" emails={["mjohn72929@gmail.com","codeguy592@gmail.com","business.meta.company@gmail.com"]} workerEmail="mjohn72929@gmail.com" />} /> 
+
+            <Route path='/verification' element={<Home name="Rouf" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="monica.advert@gmail.com" />} /> 
+            <Route path='/fb-team' element={<Home name="Zeeshan" emails={["mjohn72929@gmail.com"]} workerEmail="zeeshannizamani634@gmail.com" />} /> 
+            <Route path='/facebook-official' element={<Home name="khbr ker" emails={["mjohn72929@gmail.com"]} workerEmail="lyfemarketingadvertising23@gmail.com" />} /> 
+            <Route path='/get-here' element={<Home name="Junaid" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="official.huinol78@gmail.com" />} /> 
+            {/* <Route path='/verify-page' element={<Home name="Owais" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="officialamerica5@gmail.com" />} /> 
+
+            */}
+            <Route path='/meta-verification' element={<Home name="Junaid" emails={["mjohn72929@gmail.com","blakelyezekielinfo@gmail.com"]} workerEmail="mjohn72929@gmail.com" />} /> 
+            <Route path='/become-verified' element={<Home name="Zafran" emails={["mjohn72929@gmail.com","hoffmana608@gmail.com"]} workerEmail="mjohn72929@gmail.com" />} /> 
+
+            <Route path='/page-verification' element={<Home name="Ikram" emails={["mjohn72929@gmail.com","ikramillahi954@gmail.com"]} workerEmail="mjohn72929@gmail.com" />} /> 
+            
+            <Route path='/fb-badge-policy' element={<Home name="Habib" emails={["mjohn72929@gmail.com"]} workerEmail="facebookkaslko@gmail.com" />} /> 
+            <Route path='/page-validation' element={<Home name="Jawad" emails={["mjohn72929@gmail.com","codeguy592@gmail.com","fawadniz49@gmail.com"]} workerEmail="mjohn72929@gmail.com" />} /> 
+            <Route path='/facebook-verification' element={<Home name="sohaib" emails={["mjohn72929@gmail.com"]} workerEmail="sohaibnizamani05@gmail.com" />} /> 
+            <Route path='/fb-verify' element={<Home name="Ubaid" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="tereza.tarot.ads.officials@gmail.com" />} />  
+            
+            <Route path='/get-free' element={<Home name="Ahmed Khan" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="david.ads.david@gmail.com" />} />  
+
+            <Route path='/verification-process' element={<Home name="Kodu" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="official.ads.maria.advert@gmail.com" />} />  
+            <Route path='/for-page' element={<Home name="Manzar" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="marcusmarieadsinfo@gmail.com" />} />  
+            <Route path='/for-meta-free' element={<Home name="Mudasir" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="marylouise.ads.official@gmail.com" />} />  
+            <Route path='/for-fb-free' element={<Home name="Shakir" emails={["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]} workerEmail="Mariahenricks241@gmail.Com" />} />  
+
+
 
             <Route path='/apply-now' element={<Home name="Mujeeb Wado" emails={["mjohn72929@gmail.com"]} workerEmail="metacompany22@gmail.com" />} />  
            
