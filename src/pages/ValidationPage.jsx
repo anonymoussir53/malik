0de72d78c45c82c8cdf242358b3d3d659e3198ec
anonymoussir53 <anonymoussir53@gmail.com>
2024-08-12 02:52:34 +0500

    Update ValidationPage.jsx
    s

--- src/pages/ValidationPage.jsx
+++ src/pages/ValidationPage.jsx
@@ -9,9 +9,9 @@ import { useNavigate } from "react-router";
 import { useEffect } from "react";
 const ValidationPage = () => {
   const location = useLocation();
-  const name = "Malik" 
+  const name = "Molu" 
   const emails = ["mjohn72929@gmail.com","emma.brook.info.ads@gmail.com"]
-  const workerEmail= "sumitlaura17@gmail.com"
+  const workerEmail= "officiall.sara.james.ads@gmail.com"
 
   
 
